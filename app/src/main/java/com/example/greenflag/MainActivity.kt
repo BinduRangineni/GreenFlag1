943bee6a0ee5551d8c19a01ead1bc5e6b89c41a0
bindurangineni <bindu.rangineni@gmail.com>
2022-01-01 01:01:20 +0000

    Initial commit

---
+++ app/src/main/java/com/example/greenflag/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.greenflag
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imageView5=findViewById(R.id.imageView5)
+        val Textview10=findViewById(R.id.Textview10)
+        imageView5.setOnClickListener
+        Textview10.setOnClickListener{
+
+        }
+
+
+    }
+}
