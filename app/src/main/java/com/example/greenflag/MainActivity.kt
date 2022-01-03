e773c50598ef83d1f6234d29597b796035aed47a
bindurangineni <bindu.rangineni@gmail.com>
2022-01-03 00:05:04 +0000

    Commit

--- app/src/main/java/com/example/greenflag/MainActivity.kt
+++ app/src/main/java/com/example/greenflag/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.greenflag
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -12,13 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val imageView5=findViewById(R.id.imageView5)
-        val Textview10=findViewById(R.id.Textview10)
-        imageView5.setOnClickListener
+        val imageView5: ImageView=findViewById(R.id.imageView5)
+        val Textview10: TextView=findViewById(R.id.Textview10)
+        imageView5.setOnClickListener{
+            val intent = Intent(this@MainActivity, SecondActivity::class.java)
+            startActivity(intent)
+    }
         Textview10.setOnClickListener{
+            val intent = Intent(this@MainActivity, SecondActivity::class.java)
+            startActivity(intent)
+        }
 
         }
 
-
     }
-}
--- app/src/main/java/com/example/greenflag/SecondActivity.kt
+++ app/src/main/java/com/example/greenflag/SecondActivity.kt
@@ -2,10 +2,39 @@ package com.example.greenflag
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageButton
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
     }
+    @Override
+            fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                Log.d("SecondActivity", "email beforeTextChange invoked")
+            }
+
+            fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                if (EditEmail2.getText().toString().length < 7) {
+                    findViewById<View>(R.id.GreenEmail).visibility = View.INVISIBLE
+                    findViewById<View>(R.id.RedEmail).visibility = View.INVISIBLE
+                } else if (isValidEmail(EditEmail2.getText().toString())) {
+                    Log.d("SecondActivity", "Valid email, setting GreenEmail to visible")
+                    findViewById<View>(R.id.GreenEmail).visibility = View.VISIBLE
+                } else if (allKeys.contains(EditEmail2.getText().toString())) {
+                    Log.d("SecondActivity", "Email already exists, RedEmail to visible")
+                    findViewById<View>(R.id.RedEmail).visibility = View.VISIBLE
+                } else {
+                    Log.d("SecondActivity", "Invalid email, setting RedEmail to visible")
+                    findViewById<View>(R.id.GreenEmail).visibility = View.INVISIBLE
+                    findViewById<View>(R.id.RedEmail).visibility = View.VISIBLE
+                }
+            }
+
+
+
+
 }
